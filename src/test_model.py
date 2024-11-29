44fcfd684e6cf887b5d3653e1ca9e6cb47a040b7
Eric Buys <ebuys@uoguelph.ca>
2024-11-29 05:01:19 -0500

    Route classification

--- src/test_model.py
+++ src/test_model.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
 import os
+import shutil
 
 
 input_image = 'test3.png'
 trained_model = YOLO(f'eric_laptop_models/yolo11n_custom_trained_{15}.pt')
 result = trained_model(input_image)
 for r in result:
-    r.save(filename=f'classified_images/{input_image}')
+    shutil.copy(input_image, 'classified_images/')
+    r.save(filename=f'classified_images/{input_image[:-4]}_bbox.png')
     r.save_crop('classified_images', file_name='hold.png')
     r.save_txt(f'classified_images/{input_image[:-4]}_detection_results.txt', save_conf=False)
 # print(result)
